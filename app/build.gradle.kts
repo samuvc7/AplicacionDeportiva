0f46e195e37b643da55ca6e48cd03deb0539f6c6
Samuel <svaldehita@gmail.com>
2023-11-07 13:31:12 +0100

    Si

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.aplicaciondeportiva"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.aplicaciondeportiva"
